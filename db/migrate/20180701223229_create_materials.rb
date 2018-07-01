90e1ee4ca666f21a31920d99876459de763a4862
Marcos Vicente <marcos.vicente@wakai.com.br>
2018-07-01 19:45:12 -0300

    change migrate materials

--- db/migrate/20180701223229_create_materials.rb
+++ db/migrate/20180701223229_create_materials.rb
@@ -2,7 +2,7 @@ class CreateMaterials < ActiveRecord::Migration[5.1]
   def change
     create_table :materials do |t|
       t.string :name
-
+      t.integer :quantify
       t.timestamps
     end
   end
