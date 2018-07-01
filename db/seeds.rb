f566a2a323926b2b80de5148476f603ddcebfdef
Marcos Vicente <marcos.vicente@wakai.com.br>
2018-07-01 19:29:20 -0300

    add seed for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(email: 'marcosvicentesiqueira@gmail.com', password: 'initial123')
+if user.save!
+  puts "User marcosvicentesiqueira@gmail.com can be create"
+end
